5f28c6d6a986482c24719ba751425d004f12b391
Xime <xebertz@campus.ungs.edu.ar>
2025-06-21 17:44:06 -0300

    Add definition of simuladorMT

---
+++ sql/simuladorMT.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE FUNCTION simuladorMT(programa TEXT) RETURNS VOID AS $$
+DECLARE
+    v_programa TEXT;
+    estado_actual TEXT;
+    caracter_actual CHAR(1);
+    movimiento RECORD;
+BEGIN
+
+END;
+$$ LANGUAGE plpgsql;
