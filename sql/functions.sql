86b8faee28d98d97608764023a828e72d0eaa49b
Lucia <127611277+LuciaSoria5@users.noreply.github.com>
2025-06-22 12:53:41 -0300

    Refactor: Delete cinta_despues

--- sql/functions.sql
+++ sql/functions.sql
@@ -17,8 +17,8 @@ BEGIN
         SELECT 1 FROM regexp_split_to_table(input_string, '') AS c
         WHERE NOT EXISTS (SELECT 1 FROM alfabeto WHERE caracter = c) AND c != blanco
     ) THEN
-        INSERT INTO traza_ejecucion (estado_actual, cinta_antes, posicion_cabezal, caracter_leido, caracter_escrito, desplazamiento_realizado, cinta_despues, es_estado_final, string_aceptado)
-        VALUES ('ERROR', input_string, 0, '', '', '', '', TRUE, FALSE);
+        INSERT INTO traza_ejecucion (estado_actual, cinta_antes, posicion_cabezal, caracter_leido, caracter_escrito, desplazamiento_realizado, es_estado_final, string_aceptado)
+        VALUES ('ERROR', input_string, 0, '', '', '', TRUE, FALSE);
         RETURN;
     END IF;
 
@@ -43,25 +43,15 @@ BEGIN
 
         caracter_leido := cinta[posicion_cabezal];
 
-        -- Buscar la transición en la tabla 'programa' 
         SELECT estado_nue, caracter_nue, desplazamiento
         INTO transicion
         FROM programa
         WHERE estado_ori = estado_actual AND caracter_ori = caracter_leido;
 
-        --  Detectar cuando la máquina se apaga
         esta_detenida := transicion.estado_nue IS NULL;
 
-        -- Grabar el movimiento antes de ejecutarlo 
         INSERT INTO traza_ejecucion
-        VALUES (DEFAULT, estado_actual, array_to_string(cinta, ''), posicion_cabezal, caracter_leido, transicion.caracter_nue, transicion.desplazamiento, '', FALSE, NOT esta_detenida); -- cinta_despues se actualiza después
-
-        IF esta_detenida THEN
-            UPDATE traza_ejecucion
-            SET cinta_despues = array_to_string(cinta, '')
-            WHERE id_movimiento = (SELECT MAX(id_movimiento) FROM traza_ejecucion);
-            EXIT;
-        END IF;
+        VALUES (DEFAULT, estado_actual, array_to_string(cinta, ''), posicion_cabezal, caracter_leido, transicion.caracter_nue, transicion.desplazamiento, FALSE, NOT esta_detenida);
 
         IF estado_actual = 'qF' OR estado_actual = 'qT' THEN
             EXIT;
@@ -75,11 +65,7 @@ BEGIN
         ELSIF transicion.desplazamiento = 'L' THEN
             posicion_cabezal := posicion_cabezal - 1;
         END IF;
-
-        UPDATE traza_ejecucion
-        SET cinta_despues = array_to_string(cinta, '')
-        WHERE id_movimiento = (SELECT MAX(id_movimiento) FROM traza_ejecucion);
-
+        
     END LOOP;
 
     IF estado_actual = 'qF' THEN
--- sql/test_unitarios.sql
+++ sql/test_unitarios.sql
@@ -1,20 +1,25 @@
 SELECT '------------- Ejecutando Tests de Programa 1: w = a(a**r) -------------' AS test_info;
-SELECT '--- Ejecutando Test 1: Cadena "110011" ---' AS test_info;
+
+\i sql/programas/programa_1.sql
+
+SELECT '--- Ejecutando Test 1: Cadena "110011" ---' AS test_1;
 SELECT simuladorMT('110011');
 
-SELECT estado_actual, cinta_antes, posicion_cabezal, caracter_leido, caracter_escrito, desplazamiento_realizado, cinta_despues, es_estado_final, string_aceptado
+SELECT estado_actual, cinta_antes, posicion_cabezal, caracter_leido, caracter_escrito, desplazamiento_realizado, es_estado_final, string_aceptado
 FROM traza_ejecucion
 ORDER BY id_movimiento;
 
 SELECT 'Resultado final del Test 1 - String aceptado: ' || string_aceptado FROM traza_ejecucion ORDER BY id_movimiento DESC LIMIT 1;
-SELECT * FROM obtenerDIs();
+SELECT * FROM obtenerDIs() AS resultado_1;
 
-SELECT '--- Ejecutando Test 2: Cadena "11001" ---' AS test_info;
+SELECT '--- Ejecutando Test 2: Cadena "11001" ---';
 SELECT simuladorMT('11001');
 
-SELECT estado_actual, cinta_antes, posicion_cabezal, caracter_leido, caracter_escrito, desplazamiento_realizado, cinta_despues, es_estado_final, string_aceptado
+SELECT estado_actual, cinta_antes, posicion_cabezal, caracter_leido, caracter_escrito, desplazamiento_realizado, es_estado_final, string_aceptado
 FROM traza_ejecucion
 ORDER BY id_movimiento;
 
 SELECT 'Resultado final del Test 2 - String aceptado: ' || string_aceptado FROM traza_ejecucion ORDER BY id_movimiento DESC LIMIT 1;
 SELECT * FROM obtenerDIs();
+
+\c postgres
