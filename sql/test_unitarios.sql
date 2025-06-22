26afd3025a1e942afc79332e2e4b0e005e3904d3
Lucia <127611277+LuciaSoria5@users.noreply.github.com>
2025-06-22 12:04:32 -0300

    Add unit test 1

---
+++ sql/test_unitarios.sql
@@ -0,0 +1,11 @@
+
+SELECT '--- Ejecutando Test 1: Cadena "110011" ---' AS test_info;
+SELECT simuladorMT('110011');
+
+SELECT paso, estado_actual, cinta_antes, posicion_cabezal, caracter_leido, caracter_escrito, desplazamiento_realizado, cinta_despues, es_estado_final, string_aceptado
+FROM traza_ejecucion
+ORDER BY paso;
+
+SELECT 'Resultado final del Test 1 - String aceptado: ' || string_aceptado FROM traza_ejecucion ORDER BY id_movimiento DESC LIMIT 1;
+SELECT * FROM obtenerDIs();
+
