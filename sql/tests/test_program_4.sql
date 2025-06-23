30196967bd0b955071efb61bd2bf0f98439d0e13
Lucia <127611277+LuciaSoria5@users.noreply.github.com>
2025-06-23 17:16:38 -0300

    Add test_program_4

--- sql/tests/test_program_4.sql
+++ sql/tests/test_program_4.sql
@@ -1,11 +1,26 @@
-SELECT '------------- Ejecutando Tests de Programa 1: w = a(a**r) -------------' AS test_info;
+DROP TABLE IF EXISTS pruebas_temp;
+CREATE TEMP TABLE pruebas_temp (
+    expresion TEXT,
+    suma TEXT
+);
 
-SELECT '--- Ejecutando Test 1: Cadena "" ---' AS test_1;
-SELECT simuladorMT('1110#1110#');
-SELECT * FROM obtenerMovimientos();
-SELECT 'Resultado final del Test 1 - String aceptado: ' || string_aceptado FROM traza_ejecucion ORDER BY id_movimiento DESC LIMIT 1;
-SELECT * FROM obtenerDIs();
+-- Casos de pruebas
+INSERT INTO pruebas_temp VALUES
 
--- Anda con:
--- 10+10, 100+100, 100+101, 101+100, 101+111, 111+111, 
+('1#0#', '0001'),
+('1#1#', '0010'),
 
+('01#10#', '000011'),
+('10#10#', '000100'),
+('11#11#', '000110'),
+
+('100#100#', '00001000'),
+('100#101#', '00001001'),
+('101#100#', '00001001'),
+('101#111#', '00001100'),
+('111#111#', '00001110');
+
+-- Testing
+CALL testMTB();
+
+DROP TABLE IF EXISTS pruebas_temp;
