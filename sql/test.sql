67f2a5f6285df76947f5f7369d97b7eeb059c7a8
Lucia <127611277+LuciaSoria5@users.noreply.github.com>
2025-06-23 19:25:12 -0300

    Delete test.sql

--- sql/test.sql
+++
@@ -1,8 +0,0 @@
-SELECT simuladorMT('(a)');
-
-SELECT estado_actual FROM traza_ejecucion WHERE id_movimiento = (SELECT MAX(id_movimiento) FROM traza_ejecucion);
-
-SELECT * FROM traza_ejecucion;
-
-SELECT obtenerDIs();
-
