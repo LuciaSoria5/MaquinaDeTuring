6cafb20bbea6a0e4ebed7a0bd90ffb90dd3aa7c6
Lucia <127611277+LuciaSoria5@users.noreply.github.com>
2025-06-23 19:08:46 -0300

    Separate all tests

---
+++ sql/scripts/execute_tests_program_3.sql
@@ -0,0 +1,11 @@
+\i sql/schema.sql
+
+\c maquina_turing
+
+\i sql/data.sql
+\i sql/functions.sql
+
+\i sql/programas/program_3.sql
+\i sql/tests/test_program_3.sql
+
+\c postgres
---
+++ sql/scripts/execute_tests_program_4.sql
@@ -0,0 +1,11 @@
+\i sql/schema.sql
+
+\c maquina_turing
+
+\i sql/data.sql
+\i sql/functions.sql
+
+\i sql/programas/program_4.sql
+\i sql/tests/test_program_4.sql
+
+\c postgres
