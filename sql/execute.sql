b050408177e5c2ed087b5465198cd99c5527d733
Lucia <127611277+LuciaSoria5@users.noreply.github.com>
2025-06-23 17:19:32 -0300

    Add programs and test into execute

--- sql/execute.sql
+++ sql/execute.sql
@@ -5,8 +5,15 @@
 \i sql/data.sql
 \i sql/functions.sql
 
--- \i sql/programas/program_1.sql
--- \i sql/tests/test_program_1.sql
+\i sql/programas/program_1.sql
+\i sql/tests/test_program_1.sql
+
+\i sql/programas/program_2.sql
+\i sql/tests/test_program_2.sql
+
+\i sql/programas/program_3.sql
+\i sql/tests/test_program_3.sql
+
 \i sql/programas/program_4.sql
 \i sql/tests/test_program_4.sql
 
